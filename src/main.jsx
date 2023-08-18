00897cf6be92bc1ba666eac490b1b4f828a6b9c7
James Vicuna <jamesvicuna@Jamess-Air.lan>
2023-08-17 21:31:45 -0700

    git maintenance

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Provider } from 'react-redux'
